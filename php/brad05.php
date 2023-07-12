3daab311d9557d2588fa32833f306b2cde27ce04
bradchao <brad@brad.tw>
2023-07-12 14:46:24 +0800

    07121446

---
+++ php/brad05.php
@@ -0,0 +1,9 @@
+<?php
+    $var1 = 147; $var2 = 11;
+    $var3 = (int)($var1 / $var2);
+    $var4 = $var1 % $var2;
+    echo "{$var1} / {$var2} = {$var3} ...... {$var4}<br />"; 
+    $weight = 80;
+    echo "{$weight}kg";
+
+?>
