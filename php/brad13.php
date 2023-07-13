f9af9995d3bda378059489deda91230a8c6b1195
bradchao <brad@brad.tw>
2023-07-13 11:05:13 +0800

    07131105

--- php/brad13.php
+++ php/brad13.php
@@ -1,6 +1,6 @@
 <?php
     $month = rand(1,12);
-    $days = 31;
+    $days = -1;
     switch($month){
         case 1: case 3: case 5: case 7:
         case 8: case 10: case 12:
@@ -13,8 +13,6 @@
         case 4: case 6: case 9: case 11:
             $days = 30;
             break;
-        default:
-            $days = -1;
     }
     echo "{$month}月有{$days}天";
 ?>
