01a8f791eb7b9c599c7f2bcbdd4b7421eac8e21b
bradchao <brad@brad.tw>
2023-07-13 12:00:35 +0800

    07131200

--- php/brad99.php
+++ php/brad99.php
@@ -1,47 +1,22 @@
 <table width="100%" border="1">
-    <tr>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-    </tr>
+    <?php
+        define('ROWS', 2);
+        define('COLS', 4);
+        define('START', 2);
 
-    <tr>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-    </tr>
+        for($k=0; $k<ROWS; $k++){
+            echo '<tr>';
+            for ($j = START; $j <= (START+COLS-1); $j++){
+                $newj = $j + $k * COLS;
+                echo '<td bgcolor="pink">';
+                for ($i = 1; $i <= 9; $i++){
+                    $v = $newj * $i;
+                    echo "{$newj} x {$i} = {$v}<br />";
+                }
+                echo '</td>';
+            }
+            echo '</tr>';
+    
+        } 
+    ?>
 </table>
