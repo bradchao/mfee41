13c59c2687d566cd865312bc2c98f71498ee495f
bradchao <brad@brad.tw>
2023-07-13 10:09:32 +0800

    07131009

---
+++ php/brad12.php
@@ -0,0 +1,19 @@
+<?php
+    $a = 101;
+    switch ($a){
+        case 1:
+            echo 'A';
+            break;
+        case 10:
+            echo 'B';
+            //break;
+        default:
+            echo 'X';
+            //break;
+
+        case 100:
+            echo 'C';
+            break;
+    }
+    echo '<hr />';
+?>
