258d3f51fc87364cbd038bef11d12220dc929cf1
bradchao <brad@brad.tw>
2023-07-17 10:18:00 +0800

    07171017

--- php/pokerv2.php
+++ php/pokerv2.php
@@ -12,13 +12,20 @@
     //echo '<hr />';
 
 ?>
-
+<hr />
 <table border="1" width="100%">
     <?php
+        $shapes = ['&spades;', '<font color=red>&hearts;</font>', 
+                '<font color=red>&diams;</font>', '&clubs;'];
+        $values = ['A',2,3,4,5,6,7,8,9,10,
+                    'J','Q','K'];
         foreach($players as $player){
+            sort($player);
             echo '<tr>';
             foreach($player as $card){
-                echo "<td>{$card}</td>";
+                $s = $shapes[(int)$card/13];
+                $v = $values[$card%13];
+                echo "<td>{$s}{$v}</td>";
             }
             echo '</tr>';    
         }
