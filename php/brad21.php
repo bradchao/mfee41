c291acdb14035a5fc87c740d2ffe221f9e796c0c
bradchao <brad@brad.tw>
2023-07-17 14:32:47 +0800

    07171432

--- php/brad21.php
+++ php/brad21.php
@@ -1,17 +1,7 @@
 <?php
-    function checkTWId($id){
-        $ret = false;
-        if (strlen($id) == 10){
-            $c1 = substr($id, 0, 1);
-            $letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            if (strpos($letters, $c1)!==false){
-                $ret = true;
-            }
-        }
-        return $ret;
-    }
+    include 'bradapis.php';
 
-    if (checkTWId('X123456789')){
+    if (checkTWId('A123456789')){
         echo 'OK';
     }else{
         echo 'XX';
