445e30f03d26a5e5de73ac68337b7a6bc697e98d
bradchao <brad@brad.tw>
2023-07-19 11:44:51 +0800

    07191144

--- php/brad39.php
+++ php/brad39.php
@@ -1,8 +1,7 @@
 <?php
     // https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx
 
-    $json = file_get_contents('https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx
-    ');
+    $json = file_get_contents('https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx');
     //echo $json;
 
     $data = json_decode($json, false);
@@ -10,8 +9,15 @@
 
     $mysqli = new mysqli('localhost','root','root', 'iii', 3306);
     $mysqli->set_charset('utf8');
+    $sql = 'INSERT INTO food (name,tel,addr,hostwords,city,town,pic,lat,lng) '.
+            'VALUES (?,?,?,?,?,?,?,?,?)';
+    $stmt = $mysqli->prepare($sql);
     foreach( $data as $k => $row){
-        echo "{$k} : {$row->ID} : {$row->Name}<br />";
+        echo "{$k} : {$row->City} : {$row->Name}<br />";
+        $stmt->bind_param('sssssssdd',$row->Name,$row->Tel,$row->Address,
+        $row->HostWords,$row->City,$row->Town,
+        $row->PicURL,$row->Latitude,$row->Longitude);
+        $stmt->execute();
     }
 
 ?>
