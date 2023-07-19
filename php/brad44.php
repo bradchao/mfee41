1c7ff94695b0afa6a390705d8d143acd42802fcc
bradchao <brad@brad.tw>
2023-07-19 15:31:25 +0800

    07191531

--- php/brad44.php
+++ php/brad44.php
@@ -1,9 +1,23 @@
 <meta charset="utf-8" />
 <?php
-    $account = $_POST['account'];
-    $passwd = $_POST['passwd'];
-    $realname = $_POST['realname'];
+    $account = $_REQUEST['account'];
+    $passwd = password_hash($_REQUEST['passwd'], PASSWORD_DEFAULT) ;
+    $realname = $_REQUEST['realname'];
 
-    echo "{$account}:{$passwd}:{$realname}";
+    $mysqli = new mysqli('localhost','root','root', 'iii', 3306);
+    $mysqli->set_charset('utf8');
+
+    $sql = 'INSERT INTO member (account,passwd,realname) ' .
+            'VALUES (?,?,?)';
+    
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param('sss', $account, $passwd, $realname);
+    if ($stmt->execute()){
+        echo 'Register success';
+    }else{
+        echo 'Register failure';
+    }
+
+
 
 ?>
